fe5976566be3863683e480d8e759c037f91995f8
wruqlwx <asmirnova107@gmail.com>
2026-03-22 12:43:32 +0300

    Попытка №1. Поменяла package.json и webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,13 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
+const { execSync } = require('child_process');
+
+try {
+	console.log('--- ПРИНУДИТЕЛЬНАЯ УСТАНОВКА PLAYWRIGHT ---');
+	execSync('npx playwright install chromium', { stdio: 'inherit' });
+} catch (e) {
+	console.warn('--- НЕ УДАЛОСЬ УСТАНОВИТЬ БРАУЗЕР (ОШИБКА ИГНОРИРУЕТСЯ) ---');
+}
 
 module.exports = (envVars) => {
 	const { env } = envVars;
