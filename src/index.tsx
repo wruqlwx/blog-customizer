48eb7cfa55a3882e6ab945c75f8d6271ea892920
wruqlwx <asmirnova107@gmail.com>
2026-03-22 10:12:05 +0300

    Попытка №1. Откатили...

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { StrictMode } from 'react';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 
