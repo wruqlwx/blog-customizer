230bf86ca7d452fc6cd2ddcffd85c6070a9f42d0
wruqlwx <asmirnova107@gmail.com>
2026-03-22 22:24:04 +0300

    Попытка №1. Ну сколько можно уже 5

--- src/ui/radio-group/Option.tsx
+++ src/ui/radio-group/Option.tsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import { OptionType } from 'src/constants/articleProps';
 import { Text } from 'src/ui/text';
 import { useEnterSubmit } from './hooks/useEnterSubmit';
+
 import styles from './RadioGroup.module.scss';
 
 type OptionProps = {
@@ -18,6 +19,7 @@ export const Option = (props: OptionProps) => {
 	const optionRef = useRef<HTMLDivElement>(null);
 
 	const handleChange = () => onChange?.(option);
+
 	useEnterSubmit({ onChange, option });
 
 	const isChecked = value === selected.value;
